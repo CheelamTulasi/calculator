0bde793b24167652ee66aceb15ec7342b0859953
Yellanki Rasagna <rasagnasrinivas23@gmail.com>
2026-03-12 14:30:04 +0530

    adding to rasagna branch

---
+++ com/add/Addr.java
@@ -0,0 +1,13 @@
+package com.add;
+
+public class Addr {
+    
+    public static void main(String[] args) {
+        int a=10;
+        int b=50;
+        int c=a+b;
+        System.out.println("add of 2 numbers"+c);
+    }
+}
+    
+    
