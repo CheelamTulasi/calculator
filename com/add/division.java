a8a04b197c5340b9d41f2aace02d29e2d198887b
CheelamTulasi <144583659+CheelamTulasi@users.noreply.github.com>
2026-04-01 14:28:27 +0530

    Delete com directory

--- com/add/division.java
+++
@@ -1,9 +0,0 @@
-package com.add;
-public class division {
-    public static void main(String args[]) {
-        int a = 100;
-        int b = 20;
-        int c = a/b;
-        System.out.println("The division of two numbers");
-    }
-}
